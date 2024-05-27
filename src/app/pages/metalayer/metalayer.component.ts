7bf5be9ce0ae5c66a8fc37f6ab598db5f1180951
Arthur A. Selimov <am35a.piston@gmail.com>
2024-05-27 21:03:06 +0300

    added connections for metalauers

--- src/app/pages/metalayer/metalayer.component.ts
+++ src/app/pages/metalayer/metalayer.component.ts
@@ -9,8 +9,11 @@ import { NzContentComponent } from 'ng-zorro-antd/layout';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-
-
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 @Component({
   selector: 'app-metalayer',
@@ -27,15 +30,61 @@ import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
     NzContentComponent,
     NzSpaceModule,
     NzButtonModule,
-    NzDropDownModule
+    NzDropDownModule,
+    NzModalModule,
+    NzTableModule,
+    NzSwitchModule,
+    NzSelectModule,
+    NzDividerModule
   ],
   templateUrl: './metalayer.component.html',
-  styleUrls: ['./metalayer.component.less']
+  styleUrls: ['./metalayer.component.less'],
 })
+
+
 export class MetalayerComponent implements OnInit {
 
   ngOnInit() {
     console.log("metalayer inited")
   }
+
+  // Модальное окно связей - начало
+  isModalLinkVisible = false;
+  constructor() {}
+  showModalLink(): void {
+    this.isModalLinkVisible = true;
+  }
+  handleModalLinkOk(): void {
+    console.log('Button ok clicked!');
+    this.isModalLinkVisible = false;
+  }
+  handleModalLinkCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isModalLinkVisible = false;
+  }
+  // конец
+
+  // Таблица связей - начало
+  person = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
+  // конец
 
 }
