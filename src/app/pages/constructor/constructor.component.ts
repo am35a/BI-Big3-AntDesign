8fb133b34aee182d9474717f515df7f6ade0cd47
Arthur A. Selimov <am35a.piston@gmail.com>
2024-06-04 22:40:26 +0300

    fixed layout of block settings

--- src/app/pages/constructor/constructor.component.ts
+++ src/app/pages/constructor/constructor.component.ts
@@ -22,6 +22,7 @@ import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzListModule } from 'ng-zorro-antd/list';
 
 @Component({
   selector: 'app-constructor',
@@ -52,6 +53,7 @@ import { NzCollapseModule } from 'ng-zorro-antd/collapse';
     NzCheckboxModule,
     NzModalModule,
     NzCollapseModule,
+    NzListModule
   ],
   templateUrl: './constructor.component.html',
   styleUrls: ['./constructor.component.less']
